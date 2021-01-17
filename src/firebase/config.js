41007ba53835025d1d042fceb1fa01ad1e7fc06d
Matt Morris <36605894+matthew-f-morris@users.noreply.github.com>
2021-01-17 20:32:40 +0000

    st

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCK49Fi-JMsrGzY6moZldJgPX3vS23yYPY",
-    authDomain: "togglew-acee3.firebaseapp.com",
-    projectId: "togglew-acee3",
-    storageBucket: "togglew-acee3.appspot.com",
-    messagingSenderId: "227891329781",
-    appId: "1:227891329781:web:ce851558c7a13344f3e3bc",
-    measurementId: "G-K37TC3R841"
-  };
