27b9a9992430707146b1370889add1024d14d723
matthew-f-morris <matthew.f.morris@gmail.com>
2021-02-01 17:17:00 +0000

    Make Github routing work 2

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   }, [dispatch, state.firebaseApp, state.user]);
 
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL + '/'}>
       <div className="App">
         <Navbar />
         <Routes />
